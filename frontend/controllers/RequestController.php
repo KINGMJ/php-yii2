152d8401dd416b9b996e13012fb4ab70c0d9788e
KINGMJ <328047478@qq.com>
2022-03-04 10:51:36 +0800

    request 的相关内容

---
+++ frontend/controllers/RequestController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace frontend\controllers;
+
+
+use yii\web\Controller;
+
+// request 的相关内容
+class RequestController extends Controller
+{
+	public function actionIndex() {
+
+	}
+}
