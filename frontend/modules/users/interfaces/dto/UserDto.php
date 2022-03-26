ea10c349d61d19f22c2565942f890d7bc978dfd9
KINGMJ <328047478@qq.com>
2022-03-26 17:11:27 +0800

    用户DTO修改

--- frontend/modules/users/interfaces/dto/UserDto.php
+++ frontend/modules/users/interfaces/dto/UserDto.php
@@ -1,12 +1,16 @@
 <?php
 
-
 namespace frontend\modules\users\interfaces\dto;
 
 use yii\base\Model;
 use yii\validators\StringValidator;
 use yii\validators\UrlValidator;
 
+/**
+ * 用户传输对象
+ * Class UserDto
+ * @package frontend\modules\users\interfaces\dto
+ */
 class UserDto extends Model {
 
 	public $user_id;
@@ -18,7 +22,7 @@ class UserDto extends Model {
 
 	public function rules(): array {
 		return [
-			[['user_id'] , 'required'] ,
+			['user_id' , 'number'] ,
 			['email' , 'email'] ,
 			['phone_number' , 'number'] ,
 			['nick_name' , 'string'] ,
@@ -32,31 +36,31 @@ class UserDto extends Model {
 	 * @param $params
 	 */
 	public function validateAvatar($attribute , $params) {
-		if ( ! is_object($this->$attribute)) {
-			$this->addError($attribute , "$attribute must be an object");
+		if ( ! is_array($this->$attribute)) {
+			$this->addError($attribute , "$attribute must be an array");
 		}
 		// 验证 avatar 内部属性
 
 		// 验证 img
 		$urlValidator = new UrlValidator();
-		if ( ! empty($this->$attribute->img) &&
-			! $urlValidator->validate($this->$attribute->img , $error)) {
-			$this->addError($attribute , "$attribute.img - $error");
+		if ( ! empty($this->$attribute["img"]) &&
+			! $urlValidator->validate($this->$attribute["img"] , $error)) {
+			$this->addError($attribute , $attribute . "[img] - $error");
 		}
 
 		// 验证 status
-		if ( ! empty($this->$attribute->status) &&
-			! in_array($this->$attribute->status , [1 , 2])
+		if ( ! empty($this->$attribute["status"]) &&
+			! in_array($this->$attribute["status"] , [1 , 2])
 		) {
-			$this->addError($attribute , "$attribute.status is not a valid value");
+			$this->addError($attribute , $attribute . "[status] is not a valid value");
 		}
 
 		// 验证 letter
 		$stringValidator = new StringValidator();
 		$stringValidator->length = 2;
-		if ( ! empty($this->$attribute->letter) &&
-			! $stringValidator->validate($this->$attribute->letter , $error)) {
-			$this->addError($attribute , "$attribute.letter is not a valid value");
+		if ( ! empty($this->$attribute["letter"]) &&
+			! $stringValidator->validate($this->$attribute["letter"] , $error)) {
+			$this->addError($attribute , $attribute . "[letter] is not a valid value");
 		}
 	}
 }
