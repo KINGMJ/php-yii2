0e4a39d6cbbe3a248249ea094a9f3dde23713cb1
KINGMJ <328047478@qq.com>
2022-03-26 19:48:29 +0800

    helper调用方式修改，利用解构赋值

--- common/helpers/function.php
+++ common/helpers/function.php
@@ -8,27 +8,21 @@
 
 /**
  * 拼装输出成功信息
+ * @example [$success,$data,$code] = response_success("")
  */
 if ( ! function_exists('response_success')) {
 	function response_success($data = ""): array {
-		return array(
-			'success' => TRUE ,
-			'data' => $data ,
-			'code' => ''
-		);
+		return array(TRUE , $data , '');
 	}
 }
 
 /**
  *  拼装输出错误信息
+ * @example [$success,$data,$code] = response_error("")
  */
 if ( ! function_exists('response_error')) {
 	function response_error($data , $code = ''): array {
-		return array(
-			'success' => FALSE ,
-			'data' => $data ,
-			'code' => $code
-		);
+		return array(FALSE , $data , $code);
 	}
 }
 
